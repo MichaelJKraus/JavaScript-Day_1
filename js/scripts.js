9039b993d4dfbc6f8216323fc28c036b301b1a0e
Michael J. Kraus <51765320+MichaelJKraus@users.noreply.github.com>
2019-08-12 21:00:06 -0700

    Examples for first day

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+
+let question;
+function newFunct() {
+    question = prompt('What is your name good sir');
+    console.log('The  name bequested to you is: ' + question); 
+}
+
+console.log('I love Pork Pot Stickers');
+console.log('I love Butterfish');
+console.log('I love Hunting');
+console.log('I love Sushi');
+
+function grabElementAndSpitOutValue () {
+    //get element from index
+    const CONTAINER_FLUID_DIV = document.getElementById ('containerFluidDiv');
+    //write in HTML
+    CONTAINER_FLUID_DIV.innerHTML = 'Hey, I am here!';
+    //execute
+    console.log('This hit.');
+}
+
+function printToScreen() {
+    alert ('Console Log:')
+}
+
+alert ('Yo, welcome!');
+
+function BreakItUp() {
+    const QUESTION = prompt
+}
